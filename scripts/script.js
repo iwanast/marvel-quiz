78443eab1f9fec702680aa08900a681d6cf5f7e7
sammydjohari <djoharisammy@gmail.com>
2021-10-28 13:13:10 +0200

    finished pseudocode

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,11 @@
 //DECLARE global array easy variable
 //DECLARE global array hard variable
 //DECLARE global variable for difficulty
-//DECLARE global variable question counter
+//DECLARE global variable questionCounter
+//DECLARE global variable correctAnswers
+//DECLARE global variable easyAVG
+//DECLARE global variable hardAVG 
+//DECLARE two arrays for scoreGif and scoreText 
 
 
 //on page load, onclick difficulty and onclick try again from end page
@@ -13,20 +17,23 @@ function toggleLandingPage() {
 //onclick difficulty (event listener)
 function startGame(difficulty) {
 //ASSIGN difficulty to global variable
-//RESET question counter variable
+//RESET var questionCunter to 0
+//RESET var correctAnswers to 0
 //RUN toggleLandingPage()
 //RUN nextQuestion()
 }
 
 //run onload
-async function retrieveQuestionsFromFirebase() {
+async function retrieveDataFromFirebase() {
 //FETCH data from Firebase from both easy and hard arrays
-//AWAIT FETCH THEN RUN assignQuestionArrays()
+//FETCH the variables for the avg result, both easy and hard, from firebase
+//AWAIT FETCH THEN RUN assignGlobalVariablesFromFirebase()
 }
 
-async function assignQuestionArrays() {
+async function assignGlobalVariablesFromFirebase() {
 //ASSIGN the data in the easy array from firebase into the easy global variable
 //ASSIGN the data in the hard array from firebase into the hard global variable
+//ASSIGN the data in the global variables easyAVG and hardAVG 
 }
 
 function shuffleArray(difficulty) {
@@ -34,15 +41,51 @@ function shuffleArray(difficulty) {
 }
 
 //called by startGame() and onclick next question button
-function nextQuestion() {
-
+function gotoNextQuestion() {
+// if statement, compare question counter to array index
+// if (counter = 10)
+// RUN calculateAVG()
+// RUN toggleScorePage()
+// return/break 
+// else if (counter < 10)
+// take array (difficulty) index counter and insert in innerHTML 
 //ADD +1 to question counter
 }
 
+// onclick on answer, RUN hightlightAnswer() 
+function hightlightAnswer() {
+    // highlight green when clicked answer = true
+    // hightlight red when clicked answer = false
+    // if (clicked answer = true)
+    // add +1 to correctAnswers
+}
+
+function calculateAVG(difficulty) {
+    //ADD user score to easyScores or hardScores 
+    // ADD ++ to easyUsers or hardUsers 
+    //calculate average score 
+    //display average score on innerHTML 
+    //RUN saveAVGscoreToFirebase() 
+}
 
 
+function toggleScorePage() {
+// show user score, average score, and play again button in innerHTML 
+// RUN showScoreInfo()
+// toggle scorepage
+}
 
+// eventlistener
+// if play again button is clicked
+// RUN toggleScorePage() and toggleLandingPage()
 
+function saveAVGscoreToFirebase() {
+// save the global variables easyAVG and hardAVG 
+}
+
+function showScoreInfo() {
+// display scoreGif and scoreText depending on correctAnswers
+}
 
 
 
@@ -118,9 +161,37 @@ let easy = [
     }
 ]
 
-//To insert into the database
-let easyScores = (total sum of all easy scores)
-let easyUsers = (total count of all easy players)
+let hard = [
+    {
+        picture =
+        questionText =
+        relatedCharacter =
+        answers = [
+            {
+                answerText =
+                answerCorrect = true/false
+            },
+            {
+                answerText =
+                answerCorrect = true/false
+            },
+            {
+                answerText =
+                answerCorrect = true/false
+            },
+            {
+                answerText =
+                answerCorrect = true/false
+            }
+        ]
+    }
+]
 
-let hardScores = (total sum of all hard scores)
-let hardUsers = (total count of all hard players)
+//To insert into the database
+// easyAVG {
+// let easyScores = (total sum of all easy scores)
+// let easyUsers = (total count of all easy players)}
+
+// hardAVG {
+// let hardScores = (total sum of all hard scores)
+// let hardUsers = (total count of all hard players)}
