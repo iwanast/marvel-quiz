3bd399037c86a7ee5142e53a286766fa52b6091c
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2021-10-30 22:56:11 +0200

    Added firebase insert tools in tools.js
    Created a tools file and added a function that loops through a set number of times and creates a document in the selected collection for each loop.

---
+++ scripts/tools.js
@@ -0,0 +1,60 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+} from "https://www.gstatic.com/firebasejs/9.1.3/firebase-firestore.js";
+
+// Import Firebase configuration settings
+const firebaseConfig = {
+  apiKey: "AIzaSyBAjhZsNJ7urNqtWrUTam_f8_qiVdO3lhc",
+  authDomain: "marvel-quiz-616.firebaseapp.com",
+  projectId: "marvel-quiz-616",
+  storageBucket: "marvel-quiz-616.appspot.com",
+  messagingSenderId: "987419001414",
+  appId: "1:987419001414:web:e2c1132f189ba86b040047",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+
+//Run a loop to insert multiple documents into Firebase at once
+document.getElementById("loop-button").addEventListener("click", firebaseLoop);
+
+async function firebaseLoop() {
+  for (let i = 1; i <= 45; i++){
+
+    let id = "";
+    let image = "";
+    let question = "Placeholder question text";
+    let related = "Placeholder";
+    let answers = [
+      "Answer 1",
+      "Answer 2",
+      "Answer 3",
+      "Answer 4"
+    ];
+
+    if (i.toString().length == 1) {
+      id = "eq00" + i;
+      image = "./images/eq00" + i; 
+    } else if (i.toString().length == 2) {
+      id = "eq0" + i;
+      image = "./images/eq0" + i; 
+    } else if (i.toString().length == 3) {
+      id = "eq" + i;
+      image = "./images/eq" + i; 
+    }
+
+    await setDoc(doc(db, "easy-questions", id), {
+      question: question,
+      image: image,
+      related: related,
+      answers: answers
+    });
+  }
+}
