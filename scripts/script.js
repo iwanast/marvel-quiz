c01358697608d9bf8317283594b885b31c021a48
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2021-10-31 12:18:16 +0100

    Pause video on last frame script added

--- scripts/script.js
+++ scripts/script.js
@@ -147,6 +147,17 @@ function showScoreInfo() {
   //DISPLAY scoreGif and scoreText in innerHTML depending on correctAnswers
 }
 
+//Pause video as static image on last frame
+// select the video element
+var video = document.querySelector('.video');
+
+// listen for the event that fires when your video has finished playing
+video.addEventListener('ended', function() {
+    // pause the video
+    this.pause();
+    // set play time to the last frame
+    this.currentTime = this.duration;
+}, false);
 
 
 
